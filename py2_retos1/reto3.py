4759649a3ee1619a1e0b86aa824f988043f55f92
LauraValentinaG <>
2021-04-22 19:47:30 +0000

    Reto 3 ok (SM)

---
+++ py2_retos1/reto3.py
@@ -0,0 +1,12 @@
+a=5
+b=6
+
+suma=a+b
+resta=a-b
+division=a/b
+mutipli=a*b
+print("Las operaciones básicas de a y b son ")
+print("Suma= ",suma)
+print("Resta=  ",resta)
+print("División= ",division)
+print("Multiplicación= ",mutipli)
