c8a28eb2f7cb88f63e1225e38d0f2a8a498173ab
LauraValentinaG <81332925+LauraValentinaGonzalezNeira@users.noreply.github.com>
2021-08-11 22:40:39 +0000

    Voy a verificar si funciona el while y el if

---
+++ py5_matplotlib/plot_list.py
@@ -0,0 +1,11 @@
+lista = []
+num= input ("Ingrese el número que deseé")
+type(int(num))
+while num != 1:
+  if num %2 == 0:
+    num = num/2
+    lista.append(num) 
+  else:
+    num = (num*3)+1
+    lista.append(num)
+print(lista)
